455509695e33437c42c0c4353c9c8075460b10fb
Alexandre Magno <alexanmtz@gmail.com>
2012-04-12 19:20:45 -0300

    already reading a template a translating and warn the missing translations

--- docs/build/translation.js
+++ docs/build/translation.js
@@ -1,14 +1,28 @@
 #!/usr/bin/env node
 var hogan = require('hogan.js')
 var fs    = require('fs')
-var cjson = require('cjson');
+var cjson = require('cjson') , prod  = process.argv[2] == 'production', title = 'Twitter Bootstrap'
+var langchoose = process.argv[3]
 
-var language = cjson.load(__dirname + '/languages/pt-br.json');
+red   = '\033[31m';
+blue  = '\033[34m';
+reset = '\033[0m';
 
-var template = "bla {{_i}}back{{/i}} bla bla bla", 
+var language = cjson.load(__dirname + '/languages/'+langchoose+'.json');
+
+var template = fs.readFileSync(__dirname + '/../templates/layout.mustache', 'utf-8'),
+
 context = {
-     name: 'layout',
-     _i: function (k) { return language[context.name][k] }
+     name: "layout",
+     title: title,
+     _i: function (k) { 
+       if (language[context.name][k]) {
+        return language[context.name][k]  
+       } else {
+         console.log(blue + 'Missing translation to: ' + red + k + reset);
+         return k
+       } 
+     }  
   }
 var output = hogan.compile(template,{ sectionTags: [{o: '_i', c: 'i'}]}).render(context)
 
