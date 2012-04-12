254f5d381490c79633e7014928da6c15e1a03bd9
Alexandre Magno <alexanmtz@gmail.com>
2012-04-12 18:33:12 -0300

    reading language from a file

--- docs/build/translation.js
+++ docs/build/translation.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 var hogan = require('hogan.js')
-var languages = require('pt-br.json')
+var fs    = require('fs')
+var cjson = require('cjson');
+
+var language = cjson.load(__dirname + '/languages/pt-br.json');
 
 var template = "bla {{_i}}back{{/i}} bla bla bla", 
 context = {
      name: 'layout',
-     _i: function (k) { return languages[context.name][k] }
+     _i: function (k) { return language[context.name][k] }
   }
 var output = hogan.compile(template,{ sectionTags: [{o: '_i', c: 'i'}]}).render(context)
 
