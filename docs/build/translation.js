547e0f6ab3f94fc0499d653a26ff9c6cb2749778
Alexandre Magno <alexanmtz@gmail.com>
2012-04-16 20:05:57 -0300

    I'm on a way to render the missing translation, but the closing tags message still warning

--- docs/build/translation.js
+++ docs/build/translation.js
@@ -2,13 +2,15 @@
 var hogan = require('hogan.js')
 var fs    = require('fs')
 var cjson = require('cjson') , prod  = process.argv[2] == 'production', title = 'Twitter Bootstrap'
-var langchoose = process.argv[3]
+var langchoose = process.argv[3] || "pt-br"
 
 red   = '\033[31m';
 blue  = '\033[34m';
 reset = '\033[0m';
 
-var language = cjson.load(__dirname + '/languages/'+langchoose+'.json');
+var language = cjson.load(__dirname + '/languages/'+langchoose+'.json')
+
+var pages = fs.readdirSync(__dirname + '/../templates/pages')
 
 var template = fs.readFileSync(__dirname + '/../templates/layout.mustache', 'utf-8'),
 
@@ -24,6 +26,27 @@ context = {
        } 
      }  
   }
-var output = hogan.compile(template,{ sectionTags: [{o: '_i', c: 'i'}]}).render(context)
+var template = hogan.compile(template,{ sectionTags: [{o: '_i', c: 'i'}]})
 
-console.log(output)
+console.log(template)
+
+pages.forEach(function(name){
+  var page = fs.readFileSync(__dirname  + '/../templates/pages/' + name, 'utf-8')
+    , page_context = {
+       name: name,
+       title: name,
+       _i: function (k) { 
+         if (language[context.name][k]) {
+          return language[context.name][k]  
+         } else {
+           console.log(blue + 'Missing translation to: ' + red + k + reset);
+           return k
+         } 
+       }  
+     }
+  page = hogan.compile(page, { sectionTags: [{o:'_i', c:'i'}] })
+  page = template.render(context, {
+    body: page
+  })
+  console.log(page)
+})
